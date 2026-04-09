1e06497053678d5d141d6d4429fef327405f5cb4
Vladimir Stasevich <vladimirss042@gmail.com>
2026-04-09 22:23:00 +0300

    refactor: remove discovery-server in favor of k8s native discovery

--- property-service/src/main/java/com/example/propertyservice/PropertyServiceApplication.java
+++ property-service/src/main/java/com/example/propertyservice/PropertyServiceApplication.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
 public class PropertyServiceApplication {
--- review-service/src/main/java/com/example/reviewservice/client/BookingClient.java
+++ review-service/src/main/java/com/example/reviewservice/client/BookingClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "booking-service",
+        url = "${feign-client.url.booking-service:http://booking-service:8080}",
         configuration = FeignClientConfig.class,
         fallback = BookingClientFallback.class
 )
